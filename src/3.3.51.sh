f7a66844ee606cadc032b809bab50a5f7c8dd4c5
맹주영 <mjy9088@naver.com>
2022-06-06 18:08:47 +0900

    add exit code handling

--- src/3.3.51.sh
+++ src/3.3.51.sh
@@ -10,25 +10,28 @@ do
   is_first_line=1
   has_error=0
   find "$f" -type f \( -name "*.c" -o -name "*.h" \) | while read -r file; do
-    $NORMINETTE "$file" | while read -r line; do
-      if [ $is_first_line -eq 1 ]
-      then
-        is_first_line=0
-        continue
-      fi
-      for rule in $RULES_TO_SUPPRESS
-      do
-        line=$(echo "$line" | sed "s/^Error: ${rule}[[:space:]]\\{1,\\}(.*$//")
-      done
-      if [ "$line" != "" ]
-      then
-        if [ $has_error -eq 0 ]
-        then
-          echo "$file: Error!"
+    OUTPUT="$(
+      $NORMINETTE "$file" | while read -r line; do
+        if [ $is_first_line -eq 1 ]; then
+          is_first_line=0
+          continue
         fi
-        has_error=1
-        echo "$line"
-      fi
-    done
+        for rule in $RULES_TO_SUPPRESS
+        do
+          line=$(echo "$line" | sed "s/^Error: ${rule}[[:space:]]\\{1,\\}(.*$//")
+        done
+        if [ "$line" != "" ]; then
+          if [ $has_error -eq 0 ]; then
+            echo "$file: Error!"
+          fi
+          has_error=1
+          echo "$line"
+        fi
+      done
+    )"
+    echo "$OUTPUT"
+    if [ "$OUTPUT" != "" ]; then
+      exit 1
+    fi
   done
 done
--- test/3.3.51/test.sh
+++ test/3.3.51/test.sh
@@ -8,6 +8,18 @@ then
 fi
 
 (cd data/in && find . -type f -name "*.c") | cut -c 3- | sed s/\\.c\$// | while read -r file; do
-  $NORMINETTE "data/in/$file.c" | cmp "data/out/$file.before.txt"
-  RULES_TO_SUPPRESS=$(cat "data/in/$file.rules.txt") sh ../../src/3.3.51.sh "data/in/$file.c" | cmp "data/out/$file.after.txt"
+  echo "Testing for $file.c"
+  ERROR=0
+  OUTPUT="$($NORMINETTE "data/in/$file.c")" || ERROR=1
+  echo "$OUTPUT" | diff "data/out/$file.before.txt" -
+  NEW_ERROR=0
+  OUTPUT="$(RULES_TO_SUPPRESS="$(cat "data/in/$file.rules.txt")" sh ../../src/3.3.51.sh "data/in/$file.c")" || NEW_ERROR=1
+  echo "$OUTPUT" | diff "data/out/$file.after.txt" -
+  if [ "$OUTPUT" = "" ]; then
+    ERROR=0
+  fi
+  if [ $ERROR -ne $NEW_ERROR ]; then
+    echo "Exit code mismatch"
+    exit 1
+  fi
 done
