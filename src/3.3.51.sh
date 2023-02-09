cc314ed44866f3be23213e1053f9d56393965014
Juyeong Maing <mjy9088@naver.com>
2023-02-09 22:19:41 +0900

    Update 3.3.51.sh

--- src/3.3.51.sh
+++ src/3.3.51.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
-if [ "$NORMINETTE" = "" ]
-then
-  NORMINETTE=norminette
-fi
-
 for f in "$@"
 do
   is_first_line=1
   has_error=0
   find "$f" -type f \( -name "*.c" -o -name "*.h" \) | while read -r file; do
     OUTPUT="$(
-      $NORMINETTE "$file" | while read -r line; do
+      ${NORMINETTE-norminette} "$file" | while read -r line; do
         if [ $is_first_line -eq 1 ]; then
           is_first_line=0
           continue
@@ -29,8 +24,8 @@ do
         fi
       done
     )"
-    echo "$OUTPUT"
     if [ "$OUTPUT" != "" ]; then
+      echo "$OUTPUT"
       exit 1
     fi
   done
